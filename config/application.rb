7ebc3da9ff5fe74f908aef00ce5c431d046ba359
Barnabas Bulpett <bbulpet@rapl202278.ingramcontent.com>
2018-07-24 17:54:36 -0400

    Add dependencies and some basic js

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,13 @@ module Gatestar
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
+    config.browserify_rails.commandline_options = [
+      '-t reactify',
+      '-extension=\'.jsx\''
+    ]
+
+    # Settings in config/environments/* take precedence over those specified
+    # here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
   end
